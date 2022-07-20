1e58ed1780df7a61a518ca2296359e5650dd6b9c
avabj <avabjwill@outlook.com>
2022-07-20 17:51:30 -0400

    Code has been reworked and all CSS complete

--- top-directory/assets/js/script.js
+++ top-directory/assets/js/script.js
@@ -3,149 +3,194 @@ var mood = `All Good, Coffee Needed`;
 
 console.log(mood);
 
-//list all questions, choices and answers
-var questions = [
-    {
-        title: "Whats blacks and white with stripes all over?",
-        choices: [`zebra`, `horse`, `penguin`],
-        answer: "zebra"
-    },
-    {
-        title: "If you work 40 hours per week from age 20-65, yopu will work about how many hours in total?",
-        choices: [`50,000`, `75,000`, `90,000`],
-        answer: "90,000"
-    },
-    {
-        title: "On average how many times will you be yubikeyed within a year?",
-        choices: [`1`, `5`, `10`],
-        answer: "10"
+var wordBlank = document.querySelector(".word-blanks");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+var chosenWord = "";
+var numBlanks = 0;
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
+
+// Arrays used to create blanks and letters on screen
+var lettersInChosenWord = [];
+var blanksLetters = [];
+
+// Array of words the user will guess
+var words = [`car`,`table`, `house`, `door`, `flower`, `tree`, `bird`];
+
+// The init function is called when the page loads 
+function init() {
+  getWins();
+  getlosses();
+}
+
+// The startGame function is called when the start button is clicked
+function startGame() {
+  isWin = false;
+  timerCount = 10;
+  // Prevents start button from being clicked when round is in progress
+  startButton.disabled = true;
+  renderBlanks()
+  startTimer()
+}
+
+// The winGame function is called when the win condition is met
+function winGame() {
+  wordBlank.textContent = "YOU WON!!!🏆 ";
+  winCounter++
+  startButton.disabled = false;
+  setWins()
+}
+
+// The loseGame function is called when timer reaches 0
+function loseGame() {
+  wordBlank.textContent = "GAME OVER";
+  loseCounter++
+  startButton.disabled = false;
+  setLosses()
+}
+
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Set tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        winGame();
+      }
     }
-]
-
-//variables to keep track of Quiz
-var currentQuestionIndex = 0;
-var time = questions.length * 15;
-var timerId;
-
-// variables to reference DOM elements 
-var questionEl = document.getElementById("questions");
-var timerEl = document.getElementById("time");
-var choicesEl = document.getElementById("choices");
-var startBtn = document.getElementById("start");
-var submitBtn = document.getElementById("submit");
-
-function startQuiz() {
-    // hide start screen
-    var startScreenEl = document.getElementById("start-screen");
-    startScreenEl.setAttribute("class", "hide");
-
-    startBtn.addEventListener('click', function(){
-
-        questionsEl.removeAttribute("class");
-
-    timerId = setInterval(clockTick, 1000);
-
-    timerEl.textContent = time;
-
-    getQuestion();
-    })
-   
-    
-}
-
-function getQuestion() {
-    // get current question object from array
-    var currentQuestion = question[currentQuestionIndex];
-
-    var titleEl = document.getElementById("quesstion-title");
-    titleEl.textContent = currentQuestion.title;
-
-    //clear previous choices
-    choicesEl.innerHTML = "";
-
-    //loop over choices 
-    currentQuestion.choices.forEach(function (choice, i) {
-        //create new button for each choice
-        var choiceNode = document.createElement('button');
-        choiceNode.setAttribute("class", "choice");
-        choiceNode.setAttribute("value", "choice");
-
-        choiceNode.textContent = i + 1 + "." + choice;
-
-        //attach click event listener to each choice
-        choiceNode.onclick = questionClick;
-
-        //display on page
-        choicesEl.appendChild(choiceNode);
-
-    });
-
-}
-
-function questionClick() {
-    // check if user guessed wrong
-    if (this.value !== questions[currentQuestionIndex].answer) {
-        // penalize time
-        time -= 15;
-
-        if (time < 0) {
-            time = 0;
-        }
-
-        // display new time on page
-        timerEl.textContent = time;
-
-        // play "wrong" sound effect
-        sfxWrong.play();
-
-        feedbackEl.textContent = "Wrong!";
-    } else {
-        // play "right" sound effect
-        sfxRight.play();
-
-        feedbackEl.textContent = "Correct!";
+    // Set tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      loseGame();
     }
+  }, 1000);
+}
 
-    // flash right/wrong feedback on page for half a second
-    feedbackEl.setAttribute("class", "feedback");
-    setTimeout(function () {
-        feedbackEl.setAttribute("class", "feedback hide");
-    }, 1000);
+// Creates blanks on screen
+function renderBlanks() {
+  // Randomly picks word from words array
+  chosenWord = words[Math.floor(Math.random() * words.length)];
+  lettersInChosenWord = chosenWord.split("");
+  numBlanks = lettersInChosenWord.length;
+  blanksLetters = []
+  // Uses loop to push blanks to blankLetters array
+  for (var i = 0; i < numBlanks; i++) {
+    blanksLetters.push("_");
+  }
+  // Converts blankLetters array into a string and renders it on the screen
+  wordBlank.textContent = blanksLetters.join(" ")
+}
 
-    // move to next question
-    currentQuestionIndex++;
+// Updates win count on screen and sets win count to client storage
+function setWins() {
+  win.textContent = winCounter;
+  localStorage.setItem("winCount", winCounter);
+}
 
-    // check if we've run out of questions
-    if (currentQuestionIndex === questions.length) {
-        quizEnd();
-    } else {
-        getQuestion();
+// Updates lose count on screen and sets lose count to client storage
+function setLosses() {
+  lose.textContent = loseCounter;
+  localStorage.setItem("loseCount", loseCounter);
+}
+
+// These functions are used by init
+function getWins() {
+  // Get stored value from client storage, if it exists
+  var storedWins = localStorage.getItem("winCount");
+  // If stored value doesn't exist, set counter to 0
+  if (storedWins === null) {
+    winCounter = 0;
+  } else {
+    // If a value is retrieved from client storage set the winCounter to that value
+    winCounter = storedWins;
+  }
+  //Render win count to page
+  win.textContent = winCounter;
+}
+
+function getlosses() {
+  var storedLosses = localStorage.getItem("loseCount");
+  if (storedLosses === null) {
+    loseCounter = 0;
+  } else {
+    loseCounter = storedLosses;
+  }
+  lose.textContent = loseCounter;
+}
+
+function checkWin() {
+  // If the word equals the blankLetters array when converted to string, set isWin to true
+  if (chosenWord === blanksLetters.join("")) {
+    // This value is used in the timer function to test if win condition is met
+    isWin = true;
+  }
+}
+
+// Tests if guessed letter is in word and renders it to the screen.
+function checkLetters(letter) {
+  var letterInWord = false;
+  for (var i = 0; i < numBlanks; i++) {
+    if (chosenWord[i] === letter) {
+      letterInWord = true;
     }
-}
-
-function quizEnd() {
-    // stop timer
-    clearInterval(timerId);
-
-    // show end screen
-    var endScreenEl = document.getElementById("end-screen");
-    endScreenEl.removeAttribute("class");
-
-    // show final score
-    var finalScoreEl = document.getElementById("final-score");
-    finalScoreEl.textContent = time;
-
-    // hide questions section
-    questionsEl.setAttribute("class", "hide");
-}
-
-function clockTick() {
-    // update time
-    time--;
-    timerEl.textContent = time;
-
-    // check if user ran out of time
-    if (time <= 0) {
-        quizEnd();
+  }
+  if (letterInWord) {
+    for (var j = 0; j < numBlanks; j++) {
+      if (chosenWord[j] === letter) {
+        blanksLetters[j] = letter;
+      }
     }
+    wordBlank.textContent = blanksLetters.join(" ");
+  }
 }
+
+// Attach event listener to document to listen for key event
+document.addEventListener("keydown", function(event) {
+  // If the count is zero, exit function
+  if (timerCount === 0) {
+    return;
+  }
+  // Convert all keys to lower case
+  var key = event.key.toLowerCase();
+  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
+  
+  // Test if key pushed is letter
+  if (alphabetNumericCharacters.includes(key)) {
+    var letterGuessed = event.key;
+    checkLetters(letterGuessed)
+    checkWin();
+  }
+});
+
+// Attach event listener to start button to call startGame function on click
+startButton.addEventListener("click", startGame);
+
+// Calls init() so that it loads when page is opened
+init();
+
+//reset button 
+var resetButton = document.querySelector(".reset-button");
+
+function resetGame() {
+  // Resets win and loss counts
+  winCounter = 0;
+  loseCounter = 0;
+  // Renders win and loss counts and sets them into client storage
+  setWins()
+  setLosses()
+}
+// Attach event listener to button to Reset 
+resetButton.addEventListener("click", resetGame);
